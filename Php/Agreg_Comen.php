6d63cf9f46c578134513b60c9f7894e1890fc001
Roberto1863 <149708662+Roberto1863@users.noreply.github.com>
2024-10-16 22:10:36 -0600

    Update Agreg_Comen.php

--- Php/Agreg_Comen.php
+++ Php/Agreg_Comen.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: https://roberto1863.github.io/Web-Basura/");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 // Conectar a la base de datos
 $conn = new mysqli('localhost', 'root', '', 'comentarios');
